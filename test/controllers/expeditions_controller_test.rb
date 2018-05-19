1a4fa6f867e7fdac4c152e287724d029a9b56dd7
Vadim <kulyba.vadim@gmail.com>
2018-05-19 18:45:02 +0300

    clear repository

--- test/controllers/expeditions_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ExpeditionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/expedition_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ExpeditionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
